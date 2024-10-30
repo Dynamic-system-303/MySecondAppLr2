8f01f2d18c60217a9b3093d16b38eb0f53af8226
Anna <safron0vaa@yandex.ru>
2024-10-30 17:47:40 +0500

    Созданный проект без данный по LR 2

---
+++ MySecondAppLr2/MySecondAppLr2/src/main/java/ru/safronova/MySecondAppLr2/MySecondAppLr2Application.java
@@ -0,0 +1,13 @@
+package ru.safronova.MySecondAppLr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecondAppLr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySecondAppLr2Application.class, args);
+	}
+
+}
---
+++ MySecondAppLr2/MySecondAppLr2/src/test/java/ru/safronova/MySecondAppLr2/MySecondAppLr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.safronova.MySecondAppLr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySecondAppLr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
